4b5dbdf581005cad0cdee7ca03eb3b5a093eefe1
Valentin Hilbig <webmaster@scylla-charybdis.com>
2006-01-25 03:31:13 +0000

    dist 1.3.1

--- socklinger.c
+++ socklinger.c
@@ -25,7 +25,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * $Log$
- * Revision 1.4  2005-10-30 03:24:22  tino
+ * Revision 1.5  2006-01-25 03:31:13  tino
+ * dist 1.3.1
+ *
+ * Revision 1.4  2005/10/30 03:24:22  tino
  * additional possibilities
  *
  * Revision 1.3  2005/08/19 04:26:39  tino
@@ -49,39 +52,35 @@
 #include "tino/getopt.h"
 #include "tino/proc.h"
 #include "tino/str.h"
+#include "tino/hup.h"
 
 #include "socklinger_version.h"
 
 static int
 socklinger(int fi, int fo, char **argv, long mypid, int nr)
 {
-  struct linger		linger;
-  int			on;
-  char			buf[BUFSIZ*10], *peer;
-  int			n, status;
-  char			*env[3];
-  pid_t			pid;
+  char	buf[BUFSIZ*10], *peer;
+  int	n, status;
+  char	*env[3];
+  pid_t	pid;
 
   /* set some default socket options
    */
-  linger.l_onoff        = 1;
-  linger.l_linger       = 65535;
-  if (setsockopt(fo, SOL_SOCKET, SO_LINGER, &linger, sizeof linger) && fo!=1)
+  if ((tino_sock_linger_err(fo, 65535) && fo!=1) ||
+      (tino_sock_rcvbuf_err(fi, 102400) && fi) ||
+      (tino_sock_sndbuf_err(fo, 102400) && fo!=1))
     return 1;
 
-  on    = 102400;
-  if (setsockopt(fi, SOL_SOCKET, SO_RCVBUF, &on, sizeof on) && fi)
-    return 1;
-  on    = 102400;
-  if (setsockopt(fo, SOL_SOCKET, SO_SNDBUF, &on, sizeof on) && fo!=1)
-    return 1;
+  tino_sock_reuse(fi, 1);
+  if (fi!=fo)
+    tino_sock_reuse(fo, 1);
 
   /* Prepare the environment
    * This is a hack today, shall be done better in future
    */
-  peer	= tino_sock_get_peername(fo);
+  peer	= tino_sock_get_peername(fi);
   if (!peer)
-    peer	= tino_sock_get_peername(fi);
+    peer	= tino_sock_get_peername(fo);
   env[0]	= tino_str_printf("SOCKLINGER_NR=%d", nr);
   env[1]	= tino_str_printf("SOCKLINGER_PEER=%s", (peer ? peer : ""));
   env[2]	= 0;
@@ -89,9 +88,11 @@ socklinger(int fi, int fo, char **argv, long mypid, int nr)
     free(peer);
   /* fork off the child
    * Warning: stderr(2) stays as it is,
-   * so it might be the socket (inetd-mode)
-   * or the tty (acceptloop mode).
+   * so it might be the socket (inetd-mode) 
+  * or the tty (acceptloop mode).
    */
+
+  tino_hup_ignore(1);
 
   printf("[%05ld-%d] run %s <%d >%d\n", mypid, nr, argv[0], fi, fo);
   pid		= tino_fork_exec(fi, fo, 2, argv, env, 1);
@@ -101,6 +102,8 @@ socklinger(int fi, int fo, char **argv, long mypid, int nr)
   free(env[0]);
   tino_wait_child(pid, -1l, &status);
   printf("[%05ld-%d] ret %d, lingering\n", mypid, nr, status);
+
+  tino_hup_ignore(0);
 
   /* Now the socket belongs to us, and nobody else
    * Shutdown the writing side,
@@ -114,8 +117,10 @@ socklinger(int fi, int fo, char **argv, long mypid, int nr)
   if (shutdown(fi, SHUT_WR))
     return 1;
 
+  000;	/* add timeout and maximum data amount read away	*/
+
   while ((n=read(fi, buf, sizeof buf))!=0)
-    if (errno!=EINTR && errno!=EAGAIN)
+    if (n<0 && (errno!=EINTR && errno!=EAGAIN))
       return 1;
   return 0;
 }
@@ -126,19 +131,23 @@ socklinger_accept(int sock, char **argv, int n)
   long	pid;
 
   pid	= getpid();
+  tino_hup_start("[%05ld-%d] HUP received", pid, n);
   for (;;)
     {
       int	fd;
 
+      tino_hup_ignore(0);
       printf("[%05ld-%d] wait %s\n", pid, n, argv[1]);
       fd	= accept(sock, NULL, NULL);
       if (fd<0)
 	{
 	  if (errno==EINTR)
 	    continue;
-	  tino_exit("accept");
+	  perror("accept");
+	  sleep(1);
+	  continue;
 	}
-      if (socklinger(fd, fd, argv+2, pid, n))
+      else if (socklinger(fd, fd, argv+2, pid, n))
         perror("socklinger");
       if (close(fd))
 	tino_exit("close");
@@ -156,7 +165,7 @@ main(int argc, char **argv)
 
   if (argc<3)
     {
-      fprintf(stderr, "Usage: %s [n@]-|[host]:port|unixsocket program [args...]\n"
+      fprintf(stderr, "Usage: %s [n@][host]:port|[n@]unixsocket|- program [args...]\n"
 	      "\tVersion " SOCKLINGER_VERSION " compiled at " __DATE__ "\n"
 	      "\tYou probably need this for simple TCP shell scripts:\n"
 	      "\tif - or '' is given, use stdout(!) as socket (inetd/tcpserver)\n"
@@ -167,7 +176,8 @@ main(int argc, char **argv)
 	      "\t   env var SOCKLINGER_PEER is set to the peer's IP:port\n"
 	      "\t2) Wait for program termination.\n"
 	      "\t3) Shutdown the stdout side of the socket.\n"
-	      "\t   Wait (linger) on stdin until EOF is received.\n", argv[0]);
+	      "\t   Wait (linger) on stdin until EOF is received.\n"
+	      , argv[0]);
       return 1;
     }
   if (!*argv[1] || !strcmp(argv[1],"-"))
@@ -191,6 +201,7 @@ main(int argc, char **argv)
       int	n;
       pid_t	pid;
 
+      000;	/* missing: if parent dies, send something like HUP to all childs	*/
       for (n=cnt; n>0; n--)
 	if ((pid=fork())==0)
 	  socklinger_accept(sock, argv, n);
