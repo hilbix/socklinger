93aa7b0ee2f684021a96c33aa7ccb8c236488f6d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2006-12-12 11:52:17 +0000

    New version with editing in the old edits to do prefork and connect

--- socklinger.c
+++ socklinger.c
@@ -25,7 +25,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * $Log$
- * Revision 1.8  2006-12-12 10:36:42  tino
+ * Revision 1.9  2006-12-12 11:52:17  tino
+ * New version with editing in the old edits to do prefork and connect
+ *
+ * Revision 1.8  2006/12/12 10:36:42  tino
  * See ChangeLog
  *
  * Revision 1.7  2006/02/09 12:53:47  tino
@@ -179,8 +182,36 @@ socklinger(int fi, int fo, char **argv, int nr, int sock)
   return 0;
 }
 
+static int
+socklinger_dosock(int n, int sock, char *connect, char **argv)
+{
+  int	fd;
+
+  if (connect)
+    {
+      note(n, *connect ? "connect %s as %s" : "connect %s", argv[1], connect);
+      fd	= tino_sock_tcp_connect(argv[1], *connect ? connect : NULL);
+      /* As the connect may bind to privilege ports
+       * end the privilege eleveation here
+       */
+      tino_privilege_end_elevation();
+    }
+  else
+    {
+      note(n, "accept %s", argv[1]);
+      fd	= accept(sock, NULL, NULL);
+    }
+  if (fd<0)
+    {
+      if (errno!=EINTR)
+	perror(note_str(0, connect ? "connect" : "accept"));
+      sleep(1);
+    }
+  return fd;
+}
+
 static void
-socklinger_accept(int sock, char **argv, int n)
+socklinger_run(int sock, char *connect, char **argv, int n)
 {
   tino_hup_start(note_str(n, "HUP received"));
   for (;;)
@@ -188,81 +219,202 @@ socklinger_accept(int sock, char **argv, int n)
       int	fd;
 
       tino_hup_ignore(0);
-      note(n, "wait %s", argv[1]);
-      fd	= accept(sock, NULL, NULL);
+      fd	= socklinger_dosock(n, sock, connect, argv);
       if (fd<0)
-	{
-	  if (errno==EINTR)
-	    continue;
-	  perror(note_str(n, "accept"));
-	  sleep(1);
-	  continue;
-	}
-      else if (socklinger(fd, fd, argv+2, n, sock))
+	continue;
+      if (socklinger(fd, fd, argv+2, n, sock))
         perror(note_str(n, "socklinger"));
       if (close(fd))
 	tino_exit(note_str(n, "close"));
     }
 }
 
+static void
+socklinger_postfork(int sock, char *connect, char **argv, int cnt)
+{
+  int	n;
+  pid_t	*pids;
+
+  000;	/* missing: if parent dies, send something like HUP to all childs	*/
+
+  pids	= tino_alloc0(cnt * sizeof *pids);
+
+  tino_hup_start(note_str(0, "HUP received"));
+  for (;;)
+    {
+      int	fd, status;
+      pid_t	pid;
+
+      tino_hup_ignore(0);
+      for (n=0; n<cnt && pids[n]; n++);
+
+      if (n>=cnt)
+	note(0, "wait for childs");
+      pid	= waitpid((pid_t)-1, &status, (n<cnt ? WNOHANG : 0));
+      if (pid==(pid_t)-1)
+	{
+	  if (errno!=EINTR && errno!=EAGAIN && errno!=ECHILD)
+	    tino_exit(note_str(0, "postfork"));
+	}
+      else if (pid)
+	{
+	  for (n=0; n<cnt; n++)
+	    if (pid==pids[n])
+	      {
+		char	*cause;
+
+		pids[n]	= 0;
+		cause	= tino_wait_child_status_string(status, NULL);
+		note(n, "child %ul %s", (unsigned long)pid, cause);
+		free(cause);
+	      }
+	  continue;
+	}
+      fd	= socklinger_dosock(n, sock, connect, argv);
+      if (fd<0)
+	continue;
+      if ((pid=fork())==0)
+	{
+	  if (sock>=0)
+	    close(sock);
+	  if (socklinger(fd, fd, argv+2, n, sock))
+	    perror(note_str(n, "socklinger"));
+	  if (close(fd))
+	    tino_exit(note_str(n, "close"));
+	  exit(0);
+	}
+      if (pid==(pid_t)-1)
+	{
+	  perror(note_str(n, "fork"));
+	  continue;
+	}
+      close(fd);
+      pids[n]	= pid;
+    }
+}
+
+static void
+socklinger_prefork(int sock, char *connect, char **argv, int cnt)
+{
+  int	n;
+  pid_t	pid;
+
+  000;	/* missing: if parent dies, send something like HUP to all childs	*/
+
+  for (n=cnt; n>0; n--)
+    if ((pid=fork())==0)
+      socklinger_run(sock, connect, argv, n);
+    else if (pid==(pid_t)-1)
+      tino_exit(note_str(0, "fork"));
+
+  close(sock);	/* socket no more needed now	*/
+
+  while ((pid=wait(NULL))==(pid_t)-1)
+    if (errno!=EINTR && errno!=EAGAIN)
+      tino_exit(note_str(0, "main-wait"));
+}
+
 /* It shall get a real commandline
+ *
+ * Due to the "connect" case and the alternate fork method this routine got too complex.
  */
 int
 main(int argc, char **argv)
 {
   int	sock;
   long	cnt;
-  char	*end;
+  char	*end, *connect;
 
   if (argc<3)
     {
-      fprintf(stderr, "Usage: %s [n@][host]:port|[n@]unixsocket|- program [args...]\n"
-	      "\tVersion " SOCKLINGER_VERSION " compiled at " __DATE__ "\n"
+      fprintf(stderr, "Usage: %s [n@[src>]][host]:port|[n@[>]]unixsocket|- program [args...]\n"
+	      "\t\tVersion " SOCKLINGER_VERSION " compiled at " __DATE__ "\n"
 	      "\tYou probably need this for simple TCP shell scripts:\n"
-	      "\tif - or '' is given, use stdout(!) as socket (inetd/tcpserver)\n"
+	      "\tif - or '' is given, use stdin/out as socket (inetd/tcpserver)\n"
 	      "\telse the given socket (TCP or unix) is opened and listened on\n"
 	      "\tand each connection is served by the progam (max. n in parallel):\n"
 	      "\t1) Exec program with args with stdin/stdout connected to socket\n"
 	      "\t   env var SOCKLINGER_NR=-1 (inetd), 0 (single) or instance-nr\n"
 	      "\t   env var SOCKLINGER_PEER is set to the peer's IP:port\n"
+	      "\t   env var SOCKLINGER_SOCK is set to the socket arg\n"
 	      "\t2) Wait for program termination.\n"
 	      "\t3) Shutdown the stdout side of the socket.\n"
 	      "\t   Wait (linger) on stdin until EOF is received.\n"
+	      "Specials:\n"
+	      "\tn<0 alternate fork method (default for CygWin).\n"
+	      "\t> does a connect instead of listen/accept.  This loops if n given.\n"
+	      "\tsrc, if given, is the address to bind to for connects.\n"
 	      , argv[0]);
       return 1;
     }
   if (!*argv[1] || !strcmp(argv[1],"-"))
     {
       tino_privilege_end_elevation();
-      socklinger(0, 1, argv+2, -1, -1);
+      if (socklinger(0, 1, argv+2, -1, -1))
+	{
+	  perror(note_str(-1, "socklinger"));
+	  return 2;
+	}
       return 0;
     }
-  if ((cnt=strtol(argv[1],&end,0))>0 && *end=='@')
-    argv[1]	= end+1;
 
-  sock	= tino_sock_tcp_listen(argv[1]);
-  /* give up effective UID
-   * against privilege escalation
-   */
-  tino_privilege_end_elevation();
-  if (cnt<=1)
-    socklinger_accept(sock, argv, 0);
+  cnt		= strtol(argv[1],&end,0);
+  if (*end=='@')
+    argv[1]	= end+1;
+  else
+    cnt	= 0;
+
+  connect	= 0;
+  sock		= -1;	/* important: must be -1 such that keepfd[0] above becomes 0.  XXX is this correct?	*/
+  if ((end=strchr(argv[1],'>'))!=0)
+    {
+      connect	= argv[1];
+      *end++	= 0;
+      argv[1]	= end;
+
+      /* This is ugly and a hack
+       *
+       * I shall rewrite this with OO
+       */
+    }
   else
     {
-      int	n;
-      pid_t	pid;
+      sock	= tino_sock_tcp_listen(argv[1]);
 
-      000;	/* missing: if parent dies, send something like HUP to all childs	*/
-      for (n=cnt; n>0; n--)
-	if ((pid=fork())==0)
-	  socklinger_accept(sock, argv, n);
-	else if (pid==(pid_t)-1)
-	  tino_exit(note_str(0, "fork"));
-      close(sock);	/* socket no more needed below	*/
-      while ((pid=wait(NULL))==(pid_t)-1)
-	if (errno!=EINTR && errno!=EAGAIN)
-	  tino_exit(note_str(0, "main-wait"));
-      tino_exit(note_str(0, "child came home"));
+      /* give up effective UID
+       * against privilege escalation
+       */
+      tino_privilege_end_elevation();
+   }
+
+  if (!cnt)
+    {
+      if (connect)
+	{
+	  /* Just do a single connect
+	   */
+	  sock	= socklinger_dosock(0, -1, connect, argv);
+	  if (sock<0)
+	    return 3;
+	  if (socklinger(sock, sock, argv+2, 0, -1))
+	    {
+	      perror(note_str(0, "socklinger"));
+	      return 2;
+	    }
+	  return 0;
+	}
+      socklinger_run(sock, connect, argv, 0);
+      return 0;
     }
+#ifdef __CYGWIN__
+  if (!connect && cnt>0)
+    cnt	= -cnt;
+#endif
+
+  if (cnt<0)
+    socklinger_postfork(sock, connect, argv, -cnt);
+  else
+    socklinger_prefork(sock, connect, argv, cnt);
+  tino_exit(note_str(0, "child came home"));
   return 0;
 }
