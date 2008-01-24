23b9d62ed949fc1b7a88de49eac052e5b4e310a5
Valentin Hilbig <webmaster@scylla-charybdis.com>
2008-01-24 23:42:38 +0000

    Improved for proxy support etc.

--- example/proxy.sh
+++ example/proxy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # $Header$
 #
@@ -13,14 +13,26 @@
 # Usage:
 #	make
 #	mkdir -p PROXY
-#	rm -rf PROXY/head
-#	./socklinger 2@:8081 example/proxy.sh &
-#	tail --follow=file PROXY/LOG PROXY/MIST
+#	rm -rf PROXY/head PROXY/tmp
+#	./socklinger -sn-2 :8081 example/proxy.sh [proxy proxyport [Proxyline]]&
+#	SP="$!"
+#	trap 'kill $SP' 0
+#	tail --retry --follow=name PROXY/LOG PROXY/MIST
 #
 # Then tell Debian to use a proxy on port 8081 of the machine
 # where socklinger runs to download Debian packages.
 # Note that you must allow 2 parallel connects, as else
 # the Debian Installer starves at one location (bug in d-i?).
+#
+# Note on PROXY, PROXYPORT and PROXYLINE:
+#
+# You can hand a proxy to this script.  The Proxyline is a line which is
+# transparently sent to to Proxy.  You can use this for user authentication.
+# This can be done with a line like:
+# "Proxy-Authorization: Basic B64STRING"
+# where B64STRING is the base64 encoded string of "user:password".
+# To calculate this string you can try something like:
+# php -r 'echo base64_encode("user:password");'; echo
 #
 # You need a directory named PROXY where the data is written to.
 # In PROXY/LOG a log is written.
@@ -35,32 +47,27 @@
 # You can only access mirrors in the domain debian.org.
 # See ### ADD ALLOWED DOMAINS HERE ### to enter more alloed domains.
 #
-# If you don't have a fast internet connectivity, downloads take too long.
-# The wget on the Debian install then times out and you will get errors.
-# (This is true for apt-get, too!  This is a bug of this script, not Debian!)
-# HOWEVER THE DOWNLOAD STILL IS RUNNING IN THE BACKGROUND, SO JUST WAIT
-# UNTIL THE PROXY SCRIPT TELLS YOU IT'S FINISHED.
-# When the download finished, you have to restart the install process!
-# This is a bit clumsy, perhaps, but after a few tries you will have
-# downloaded everything you need.  I like it as it is, as usually the
-# machine to install has no internet connectivity, and usually I repeat
-# the install several times until I am happy with it.
-#
 # It does not check for changes on the Debian mirror!  To get new files,
 # remove the header files (rm -rf PROXY/head).
 # YOU HAVE TO DO THIS MANUALLY OR BY CRON!!!
 #
 # $Log$
+# Revision 1.3  2008-01-24 23:42:38  tino
+# Improved for proxy support etc.
+#
 # Revision 1.2  2004-09-20 01:52:14  tino
 # Clarified, you must allow 2 proxies running in parallel.
-#
-# Revision 1.1  2004/08/15 22:19:37  Administrator
-# added
 
-cd "`dirname "$0"`/PROXY" || exit 1
+cd PROXY || exit 1
 umask 002
 
-exec 2>>LOG
+mkdir -p tmp
+
+PROXY="$1"
+PROXYPORT="$2"
+PROXYLINE="$3"
+
+#exec 2>>LOG
 
 mist()
 {
@@ -78,16 +85,11 @@ done
 exit 1
 }
 
-if [ 0 != "$#" ]
-then
-	url="$*"
-else
-	read -r -t 20 get url rest || mist "EOF" 
-	[ ".GET" != ".$get" ] && mist "no get"
-	case "$url" in
-	*)	url="`expr "$url" : '\(.*\).$'`";;
-	esac
-fi
+read -r -t 20 get url rest || mist "EOF" 
+[ ".GET" != ".$get" ] && mist "no get"
+case "$url" in
+*)	url="`expr "$url" : '\(.*\).$'`";;
+esac
 
 case "$url" in
 *..*)	mist ".. im URL";;
@@ -96,7 +98,7 @@ http://*)	;;
 esac
 
 host="`expr "$url" : 'http://\([0-9a-z.]*\)[:/]'`"
-port="`expr "$url" : 'http://[^:]*:\([0-9]*\)/'`"
+port="`expr "$url" : 'http://[^:/]*:\([0-9]*\)/'`"
 
 [ -z "$port" ] && port=80
 [ -z "$host" ] && mist "host?"
@@ -106,51 +108,119 @@ case "$host" in
 *)		mist "no debian";;
 esac
 
-if [ ! -s "head/$url" ]
-then
-	mkdir -p "head/`dirname "$url"`" || exit 1
-	echo "++ $url" >> LOG
-	{
-	echo "HEAD $url HTTP/1.0"
-	if [ 80 != "$port" ]
-	then
-		echo "Host: $host:$port"
-	else
-		echo "Host: $host"
-	fi
-	echo ""
-	} |
-	netcat -w60 "$host" "$port" > "head/$url"
-fi
+fhost="$host"
+fport="$port"
+[ -z "$PROXY" ] || fhost="$PROXY"
+[ -z "$PROXYPORT" ] || fport="$PROXYPORT"
 
-cat "head/$url"
-
-len=""
+: fetch
+fetch()
 {
-read response code rest || exit 1
-[ 200 = "$code" ] || exit 1
-while	read -r line || exit 1
+{
+echo "$1 $url HTTP/1.0"
+if [ 80 != "$port" ]
+then
+	echo "Host: $host:$port"
+else
+	echo "Host: $host"
+fi
+[ -z "$PROXYLINE" ] || echo "$PROXYLINE"
+echo ""
+} |
+netcat -w60 "$fhost" "$fport"
+}
+
+spcparm()
+{
+eval $1='`expr "$line" : "[^:]*:[[:space:]]*\\([^[:space:]]*\\)"`'
+}
+
+fullparm()
+{
+eval $1='`expr "$line" : "[^:]*:[[:space:]]*\\(.*\\)\\$"`'
+}
+
+: parsehead
+parsehead()
+{
+read response code rest || return 1
+[ 200 = "$code" ] || return 1
+while	line=""
+	read -r line || return 1
 	[ -n "$line" -a "" != "$line" ]
 do
 	case "$line" in
-	[Cc]ontent-[Tt]ype:*)	type="`expr "$line" : '[^:]*:[[:space:]]*\([^[:space:]]*\)'`";;
-	[Cc]ontent-[Ll]ength:*)	len="`expr "$line" : '[^:]*:[[:space:]]*\([^[:space:]]*\)'`";;
+	[Cc]ontent-[Tt]ype:*)	spcparm $1type;;
+	[Cc]ontent-[Ll]ength:*)	spcparm $1len;;
+	[Ll]ast-[Mm]odified:*)	fullparm $1date;;
 	esac
 done
-} < "head/$url"
+return 0
+}
 
-if [ -f "data/$url" ]
+: cathead
+cathead()
+{
+while	read -r line || exit 1
+	[ -n "$line" -a "" != "$line" ]
+do
+	echo "$line"
+done
+echo "$line"
+}
+
+: chklen
+chklen()
+{
+clen="`{ parsehead c; [ ":$ctype:$clen:$cdate" = ":$type:$len:$date" ] && wc -c; }`" || return 1
+[ -z "$len" -o ".$len" = ".$clen" ]
+}
+
+durl="$url"
+furl="$url"
+case "$url" in
+*/)	furl="$url""index.html";;
+*)	durl="`dirname "$url"`";;
+esac
+
+if [ -f "data/$furl" ]
 then
-	echo "-- $url" >> LOG
-else
-	mkdir -p "tmp/`dirname "$url"`" || exit 1
-	echo "do $url ($len)" >> LOG
-	curl -C- -o"tmp/$url" "$url" >>LOG
-	[ -f "tmp/$url" ] || exit 1
-	[ "$len" = "`find "tmp/$url" -printf "%s"`" ] || exit 1
-	echo "ok $url" >> LOG
-	mkdir -p "data/`dirname "$url"`" || exit 1
-	mv -f "tmp/$url" "data/$url" || exit 1
+	if [ ! -s "head/$furl" ]
+	then
+		mkdir -p "head/$durl" || exit 1
+		echo "++ $url" >> LOG
+		fetch HEAD > "head/$furl"
+	fi
+
+	len=""
+	if	{ ! [ "head/$furl" -nt "data/$furl" ]; } ||
+		{ parsehead < "head/$furl" &&
+		  chklen < "data/$furl" &&
+		  touch -r "data/$furl" "head/$furl";
+		}
+	then
+		echo "-- $url" >> LOG
+		exec cat "data/$furl"
+	fi
+
+	echo "up $url" >> LOG
+	mv -f --backup=t "head/$furl" "head/$furl.old"
 fi
 
-exec cat "data/$url"
+echo "do $url" >> LOG
+
+TMP="tmp/$$"
+fetch GET | tee "$TMP"
+
+[ -f "$TMP" ] || exit 1
+parsehead < "$TMP" || exit 1
+chklen < "$TMP" || exit 1
+
+echo "ok $url" >> LOG
+
+mkdir -p "head/$durl" || exit 1
+cathead < "$TMP" > "head/$furl"
+
+mkdir -p "data/$durl" || exit 1
+mv -f --backup=t "$TMP" "data/$furl"
+
